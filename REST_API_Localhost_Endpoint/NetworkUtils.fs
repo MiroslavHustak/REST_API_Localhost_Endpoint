e63872670e93c9286a4e8d545f55b603bac9448f
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-07-14 21:22:48 +0200

    14-07-2026

---
+++ REST_API_Localhost_Endpoint/NetworkUtils.fs
@@ -0,0 +1,15 @@
+﻿module NetworkUtils 
+
+open System.Net
+open System.Net.Sockets
+
+let getLocalIPv4 () : string =
+    
+    use socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+    socket.Connect("8.8.8.8", 65530) // no packet actually sent; just resolves routing
+    
+    match socket.LocalEndPoint with
+    | :? IPEndPoint as ep 
+        -> ep.Address.ToString()
+    | _ -> "127.0.0.1"
+
--- REST_API_Localhost_Endpoint/Program.fs
+++ REST_API_Localhost_Endpoint/Program.fs
@@ -63,10 +63,12 @@ module Program =  //Kestrel
 
         let app =
 
+            let localIp = sprintf "%s%s:5000" <| @"http://" <| NetworkUtils.getLocalIPv4 ()
+
             application 
                 {
                     use_router apiRouter
-                    url "http://192.168.1.20:5000"  //IIS hosting does not bind to IP/port inside the app, Saturn’s url setting binds Kestrel directly to a socket.
+                    url localIp  //IIS hosting does not bind to IP/port inside the app, Saturn’s url setting binds Kestrel directly to a socket.
                     memory_cache
                     use_static "static"
                     use_gzip
